141b1914634051fc8da70b2e010f85443f26073d
isaprestess <164901803+isaprestess@users.noreply.github.com>
2024-10-28 11:41:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Primeira pergunta',
+    'Quem eu sou?',
+    'Eu sou a isa, tenho 16 anos, gosto de jogar volêi e sou cristã'
+)
+
+criaCartao(
+    'Segunda pergunta',
+    'Minha família é?',
+    'Complicada, mas amorosa'
+)
+
+criaCartao(
+    'Terceira pergunta',
+    'Onde moro?',
+    'Moro em Pinhão-PR, no bairro Vila Caldas, longitude -25.707320, latitude -51.645064 '
+)
+
+criaCartao(
+    'Quarta pergunta',
+    'Onde estudo?',
+    'No colégio estadual Mario Evaldo Morski, 1979 foi fundado'
+) 
+
+criaCartao(
+    'Quinta pergunta',
+    'Minha turma',
+    'É cheia de alunos inteligentes e muitos também desinteressados'
+)
+
+criaCartao(
+    'sexta pergunta',
+    'Faculdade?',
+    'Não sei mais qual quero fazer'
+) 
+criaCartao(
+    ' setima pergunta',
+    'Minha cidade?',
+    'É pequena mais eu gosto'
+) 
+
+criaCartao(
+    ' setima pergunta',
+    'Meu sonho?',
+    'Sonho em ter uma casa confortável, e um trabalho bom'
+) 
+
+
